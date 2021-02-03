25568e7611de071bfe3bad377add8f5bdf738a85
syltruong <syltruong@gmail.com>
2021-02-03 13:49:44 +0800

    add display of coming soon messages

--- frontend/index.js
+++ frontend/index.js
@@ -18,21 +18,35 @@ async function getRandomWord() {
     }
 }
 
-function revealMeta(element) {
-    element.querySelector(".meta").style.display = "block"
-}
 
-function hideMeta(element) {
-    element.querySelector(".meta").style.display = "none"
+// Display suggestions
+
+function toggleMeta(elt) {  
+    elt.querySelector(".meta").classList.toggle("hidden")
 }
 
 const suggestions = document.getElementsByClassName("suggestion")
 
-Array.from(suggestions).forEach((element, index) => {
-    element.addEventListener("mouseenter", () => {
-        revealMeta(element);
-    })
-    element.addEventListener("mouseleave", () => {
-        hideMeta(element);
-    })
+Array.from(suggestions).forEach(elt => {
+    elt.addEventListener("mouseenter", () => toggleMeta(elt))
+    elt.addEventListener("mouseleave", () => toggleMeta(elt))
 });
+
+
+// Command handling
+
+//// Coming soon
+
+const btns = document.getElementsByClassName("comingSoon")
+const overlayDiv = document.getElementById("overlay")
+
+function toggleOverlay(){
+    overlayDiv.classList.toggle("hidden")        
+}
+
+Array.from(btns).forEach(elt => {
+    elt.addEventListener("click", toggleOverlay)
+});
+overlayDiv.addEventListener("click", toggleOverlay)
+
+//// Random
