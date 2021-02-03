4c0673c846403690a3258e30a611441647827896
syltruong <syltruong@gmail.com>
2021-02-03 13:51:03 +0800

    reorder js

--- frontend/index.js
+++ frontend/index.js
@@ -1,24 +1,3 @@
-
-const randomBtn = document.getElementById("randomBtn")
-const baseUrl = "http://0.0.0.0:5000/" 
-
-randomBtn.addEventListener("click", () => {
-    console.log("click")
-    getRandomWord()
-})
-
-async function getRandomWord() {
-    try {
-        const response = await fetch(baseUrl + "random");
-        const jsonData = await response.json();
-
-        console.log(jsonData)
-    } catch(err) {
-        console.log(err)
-    }
-}
-
-
 // Display suggestions
 
 function toggleMeta(elt) {  
@@ -50,3 +29,22 @@ Array.from(btns).forEach(elt => {
 overlayDiv.addEventListener("click", toggleOverlay)
 
 //// Random
+
+const randomBtn = document.getElementById("randomBtn")
+const baseUrl = "http://0.0.0.0:5000/" 
+
+randomBtn.addEventListener("click", () => {
+    console.log("click")
+    getRandomWord()
+})
+
+async function getRandomWord() {
+    try {
+        const response = await fetch(baseUrl + "random");
+        const jsonData = await response.json();
+
+        console.log(jsonData)
+    } catch(err) {
+        console.log(err)
+    }
+}
