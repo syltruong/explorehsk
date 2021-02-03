fe515aea8d32fcc1151d1b9375016f7405255e77
Sylvain Truong <syltruong@users.noreply.github.com>
2021-02-03 15:19:35 +0800

    Random button (#8)
    * random button ok
    
    * fix text suggestions margins

--- frontend/index.js
+++ frontend/index.js
@@ -4,12 +4,14 @@ function toggleMeta(elt) {
     elt.querySelector(".meta").classList.toggle("hidden")
 }
 
-const suggestions = document.getElementsByClassName("suggestion")
-
-Array.from(suggestions).forEach(elt => {
-    elt.addEventListener("mouseenter", () => toggleMeta(elt))
-    elt.addEventListener("mouseleave", () => toggleMeta(elt))
-});
+function makeSuggestionsHoverable() {
+    const suggestions = document.getElementsByClassName("suggestion")
+    
+    Array.from(suggestions).forEach(elt => {
+        elt.querySelector(".suggestion__word").addEventListener("mouseenter", () => toggleMeta(elt))
+        elt.querySelector(".suggestion__word").addEventListener("mouseleave", () => toggleMeta(elt))
+    });
+}
 
 
 // Command handling
@@ -31,7 +33,7 @@ overlayDiv.addEventListener("click", toggleOverlay)
 //// Random
 
 const randomBtn = document.getElementById("randomBtn")
-const baseUrl = "http://0.0.0.0:5000/" 
+const baseUrl = "http://167.99.75.225:5000/" 
 
 randomBtn.addEventListener("click", () => {
     console.log("click")
@@ -44,7 +46,65 @@ async function getRandomWord() {
         const jsonData = await response.json();
 
         console.log(jsonData)
+        populateCenter(jsonData.source)
+        populateSuggestions(jsonData.most_similar)
+
     } catch(err) {
         console.log(err)
     }
 }
+
+function populateCenter(source) {
+    centerDiv = document.getElementById("center")
+    centerDiv.querySelector(".center__word").innerText = source["Word"]
+    centerDiv.querySelector(".pinyin").innerText = source["Pronunciation"]
+    centerDiv.querySelector(".translation").innerText = source["Definition"]
+}
+
+function populateSuggestions(mostSimilar) {
+    const suggestionContainerDiv = document.getElementById("suggestion-container") 
+    
+    // reset content
+    suggestionContainerDiv.textContent = ""
+
+    // populate with suggestions
+    // we skip the first element on purpose
+    // since it is the same as source
+    for (let i=1; i<mostSimilar.length; i++){
+        const item = mostSimilar[i]
+
+        const suggestionDiv = document.createElement("div")
+        suggestionDiv.classList.add("suggestion")
+
+        const wordDiv = document.createElement("div")
+        wordDiv.classList.add("zh")
+        wordDiv.classList.add("suggestion__word")
+        wordDiv.innerText = item["Word"]
+
+        const metaDiv = document.createElement("div")
+        metaDiv.classList.add("meta") 
+        metaDiv.classList.add("hidden")
+
+        const pinyinDiv = document.createElement("div")
+        pinyinDiv.classList.add("en") 
+        pinyinDiv.classList.add("pinyin")
+        pinyinDiv.innerText = item["Pronunciation"]
+
+        const translationDiv = document.createElement("div")
+        translationDiv.classList.add("en") 
+        translationDiv.classList.add("translation")
+        translationDiv.innerText = item["Definition"]
+
+        metaDiv.appendChild(pinyinDiv)
+        metaDiv.appendChild(translationDiv)
+        suggestionDiv.appendChild(metaDiv)
+        suggestionDiv.appendChild(wordDiv)
+
+        suggestionContainerDiv.appendChild(suggestionDiv)
+
+    }
+
+    makeSuggestionsHoverable()
+}
+
+getRandomWord()
