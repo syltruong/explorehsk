19d9d716ac34b99015b3dbea3b1af0ff995f5812
syltruong <syltruong@gmail.com>
2021-04-29 12:16:11 +0800

    header styling

--- frontend-react/src/Header.js
+++ frontend-react/src/Header.js
@@ -1,17 +1,18 @@
 import github from './github.svg';
+import './Header.css';
 
 function Title() {
     return (
-        <div>
-            <h1>
+        <div className={'title'}>
+            <h1 className={'zh'}>
                 词语接龙
             </h1>
-            <div>
+            <h2 className={'en'}>
                 [explore hsk]
-            </div>
-            <div>
+            </h2>
+            <h3 className={'en'}>
                 Browse Chinese vocabulary by etymology
-            </div>
+            </h3>
         </div>
     )
 }
@@ -19,18 +20,18 @@ function Title() {
 
 function Nav() {
     return (
-        <div>
+        <nav>
             {/* think about settings those to <button></button> tags */}
-            <div>random</div> 
-            <div>search</div>
-            <div>about</div>
-            <div>settings</div>
-            <div>
+            <button className={'en'}>random</button> 
+            <button className={'en'}>search</button>
+            <button className={'en'}>about</button>
+            <button className={'en'}>settings</button>
+            <button>
                 <a href="https://github.com/syltruong/explorehsk" target="_blank">
                     <img src={github} alt="github octocat" />
                 </a>
-            </div>
-        </div>
+            </button>
+        </nav>
     )
 }
 
