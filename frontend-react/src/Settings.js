ea33e884dd6d32f33b5dd18ccfe6ebb9c5643e41
syltruong <syltruong@gmail.com>
2021-05-09 15:49:01 +0800

    operational slider

--- frontend-react/src/Settings.js
+++ frontend-react/src/Settings.js
@@ -1,9 +1,30 @@
 // import './About.css'
+import {useState} from "react"
 
 function Settings() {
+    
+    const [hskLevel, setHskLevel] = useState(4)
+   
+    const onChange = event => {
+        setHskLevel(event.target.value)
+    }
+
     return (
         <section className={"en"}>
-            My Settings
+            <h2>Settings</h2>
+            <form>
+                <label for="hsk-level-slider">
+                    HSK Level <span id="hsk-level-span">{hskLevel}</span>
+                </label>
+                <input 
+                    type="range" 
+                    min="1" 
+                    max="6" 
+                    value={hskLevel}
+                    name="hsk-level-slider" 
+                    onChange={onChange}
+                />
+            </form>
         </section>
     )
 }
