1839185054152c957b06ba5be288445d99e68594
syltruong <syltruong@gmail.com>
2021-06-16 04:23:34 +0000

    introduce deduplication func

---
+++ backend/src/data.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+def dedup_entries(words_df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Deduplicate entries in the dataset,
+    based on columns "Word" and "Pronunciation"
+
+    The "HSK Level" is set to the lowest level.
+    The "Definition" is set to be the one of the lowest level.
+
+    Parameters
+    ----------
+    words_df : pd.DataFrame
+        words dataframe, the expected columns are
+        "Word", "Pronunciation", "HSK Level"
+
+    Returns
+    -------
+    pd.DataFrame
+        words dataframe with no duplicates
+    """
+
+    df = words_df.sort_values("HSK Level")
+    df = df.drop_duplicates(["Word", "Pronunciation"], keep="first")
+    
+    ret = words_df.loc[words_df.index.isin(df.index)]
+
+    return ret
+    
